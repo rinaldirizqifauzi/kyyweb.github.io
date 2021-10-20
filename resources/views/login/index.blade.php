fb40353a416f17133775983b660320ac96a47fa8
My Web <kylavesfar25@gmail.com>
2021-10-20 11:06:32 +0700

    Menambahkan Authentication dan Middlewere

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -9,17 +9,30 @@
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         @endif
+
+        @if (session()->has('LoginError'))
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ session('LoginError') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
         <main class="form-signin">
             <h1 class="h3 mb-3 fw-normal text-center">Please Log In </h1>
-            <form>
+            <form action="/login" method="POST">
+                @csrf
                <div class="form-floating">
-                   <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                   <label for="floatingInput">Email Address</label>
+                   <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="example@gmail.com" autofocus value="{{ old('email') }}" required>
+                   <label for="email">Email</label>
+                   @error('email')
+                    <div class="invalid-feedback">
+                        {{ $message }}    
+                    </div>   
+                   @enderror
                </div>
       
                <div class="form-floating">
-                   <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                   <label for="floatingPassword">Password</label>
+                   <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+                   <label for="password">Password</label>
                </div>
                <button class="w-100 btn btn-lg btn-primary" type="submit">Log In</button>
            </form>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -19,13 +19,32 @@
             <a class="nav-link {{ ($active === "categories") ? 'active' : '' }}" href="/categories">Categories</a>
           </li>
         </ul>
+
         <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i>
-              Login
-            </a>
-          </li>
-        </ul>
+            @auth
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Wellcome, {{ auth()->user()->name }}
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-window-reverse"></i> My Dashboard</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li>
+                    <form action="/logout" method="POST">
+                      @csrf
+                      <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Log Out</button>
+                    </form>
+                </li>
+              </ul>
+            </li>
+            @else
+            <li class="nav-item">
+              <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i>
+                Login
+              </a>
+            </li>
+            @endauth
+          </ul>
       </div>
     </div>
   </nav>
