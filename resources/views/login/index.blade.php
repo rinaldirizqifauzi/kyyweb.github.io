7ea80bb5138aae842ed250e7e31491e5af9fd6bd
My Web <kylavesfar25@gmail.com>
2021-10-20 09:53:28 +0700

    Menambahkan UserRegistration

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.main')
+
+@section('container')
+<div class="row justify-content-center">
+    <div class="col-lg-5">
+        @if (session()->has('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+        <main class="form-signin">
+            <h1 class="h3 mb-3 fw-normal text-center">Please Log In </h1>
+            <form>
+               <div class="form-floating">
+                   <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                   <label for="floatingInput">Email Address</label>
+               </div>
+      
+               <div class="form-floating">
+                   <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                   <label for="floatingPassword">Password</label>
+               </div>
+               <button class="w-100 btn btn-lg btn-primary" type="submit">Log In</button>
+           </form>
+            <small class="d-block text-center mt-3">Not Registered? <a href="/register">Register Now!</a></small> 
+          </main>   
+    </div>
+</div>
+  
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Models\User;
-use App\Models\Post;
 use App\Models\Category;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 
 /*
@@ -44,3 +45,8 @@ Route::get('/categories', function(){
         'categories' => Category::all(),
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
+
